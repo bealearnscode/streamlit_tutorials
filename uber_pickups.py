919408b26e4584670bdcd583e8124b181581abe0
B <bealearnscode@gmail.com>
2021-08-05 23:32:41 -0400

    displaying raw data

--- uber_pickups.py
+++ uber_pickups.py
@@ -26,3 +26,6 @@ data = load_data(10000)
 # Notify the reader that the data was successfully loaded.
 #data_load_state.text('Loading data...done!')
 data_load_state.text("Done! (using st.cache)")
+
+st.subheader('Raw data')
+st.write(data)
